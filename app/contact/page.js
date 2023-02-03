b8a814c24b26a0e98fc798eb5ff1de2080d8f2fb
369webforall <369webforall@gmail.com>
2023-02-03 15:08:23 +0545

    contact form

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,20 @@
+'use client';
 import React from 'react';
 
 const Contact = () => {
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {};
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    fetch('api/mail', {
+      method: 'post',
+      body: JSON.stringify(formData),
+    });
+    console.log(formData);
+  };
   return (
     <section className="relative text-gray-600 mt-20" id="contact">
       <div className="container flex justify-center px-5 py-12 mx-auto items-cener sm:flex-nowrap">
@@ -11,41 +25,43 @@ const Contact = () => {
           <p className="mb-5 text-center text-purple-900">
             Any questions or remarks ? Just write us a message !
           </p>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="text-sm text-gray-600">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="text-sm text-gray-600">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="message" className="text-sm text-gray-600">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full h-32 px-3 py-1 text-base leading-6 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-            ></textarea>
-          </div>
-          <button className="px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
-            Send
-          </button>
+          <form method="post" onSubmit={handleOnSubmit}>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="text-sm text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="message" className="text-sm text-gray-600">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full h-32 px-3 py-1 text-base leading-6 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+              ></textarea>
+            </div>
+            <button className="px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
+              Send
+            </button>
+          </form>
         </div>
       </div>
     </section>
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const body = JSON.parse(req.body);
+  const message = `
+  Name: ${body.name}\r\n
+  Email: ${body.email}\r\n
+  Message: ${body.message}\r\n
+  `;
+
+  const data = {
+    to: 'admin@wecoding.info',
+    from: 'admin@webcoding.info',
+    subject: 'New Message from web',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  };
+  mail.send(data);
+  res.status(200).json({ status: 'OK' });
 }
